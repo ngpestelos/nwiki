40441d8302829b7c7a9805b818c355f0294f53ae
Nestor G Pestelos Jr <ngpestelos@gmail.com>
2009-03-21 20:22:44 +0800

    moved 'not found' message to html
    nuked the editor

--- nwiki.py
+++ nwiki.py
@@ -3,6 +3,7 @@
 import web, os
 
 urls = (
+  '/w/edit/(.*)', 'WikiEditor',
   '/w/(.*)', 'WikiPage',
   '/w', 'StartPage'
 )
@@ -11,21 +12,33 @@ startPage = 'Welcome'
 
 render = web.template.render('static/', base='site')
 
-renderBare = web.template.render('static/')
-
 # don't create docs for these pages
 #watch = ['browse', 'edit', 'about', 'home']
 
 app = web.application(urls, globals())
+
+def create(name, content):
+    pass
+
+def read(name):
+    pass
+
+def update(name, newcontent):
+    pass
+
+def delete(name):
+    pass
+
+class WikiEditor:
+    def GET(self, name):
+        return render.editor(name, '')
 
 class WikiPage:
     def GET(self, name):
         if not name:
             raise web.seeother('/w')
 
-        editUrl = os.path.join('/w/edit', name)
-        msg = '<h3 id="notfound">%s does not exist. <a href="%s">Create?</a></h3>'
-        return renderBare.site(name, msg % (name, editUrl))
+        return render.not_found(name)
 
 class StartPage:
     def GET(self):

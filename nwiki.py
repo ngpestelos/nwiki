6c0d3d8fc44cf81fbc010e335d783a6b638c1bdf
Nestor G Pestelos Jr <ngpestelos@gmail.com>
2009-03-21 19:41:48 +0800

    clean slate

--- nwiki.py
+++ nwiki.py
@@ -1,89 +1,16 @@
-# A simple CMS running on CouchDB and web.py
-
 import web
-from couchdb import Server
-from couchdb import ResourceNotFound
-import os
-from datetime import datetime
-from markdown import markdown
-
-render = web.template.render('static/')
 
 urls = (
-    '/rename', 'Rename',
-    '/edit/(.*)', 'WikiEditor',
-    '/browse', 'WikiBrowser',
-    '/(.*)', 'WikiPage'
+  '/w/(.*)', 'StartPage'
 )
 
-startPage = 'GoAway'
+startPage = 'Welcome'
 
 # don't create docs for these pages
 #watch = ['browse', 'edit', 'about', 'home']
 
-server = Server('http://localhost:5984')
-db = server['nwiki']
+app = web.application(urls, globals())
 
-app = web.application(urls, globals(), autoreload=True)
-
-class Rename:
-    def POST(self):
-        input = web.input()
-        if input.action == 'Save':
-            doc = db[input.d]
-            posted = ''
-            if 'posted' in doc:
-                posted = doc['posted']
-            db[input.new_name] = {'content' : doc['content'], \
-              'posted' : posted, 'updated' : datetime.today().ctime()}
-            del db[input.d]
-            web.redirect("/browse")
-        elif input.action == 'Cancel':
-            web.redirect("/browse")
-    def GET(self):
-        input = web.input()
-        return render.rename(input.d)
-
-class WikiBrowser:
-    def GET(self):
-        return render.browser([doc for doc in db])
-
-class WikiEditor:
+class StartPage:
     def GET(self, name):
-        try:
-            doc = db[name]
-            return render.editor(name, doc['content'])
-        except ResourceNotFound:
-            return render.editor(name, '')
-    def POST(self, name):
-        input = web.input()
-        if input.action == 'Save':
-            try:
-                doc = db[name]
-                doc['content'] = input.page
-                doc['updated'] = datetime.today().ctime()
-                db[name] = doc
-                web.redirect('/%s' % name)
-            except ResourceNotFound:
-                posted = datetime.today().ctime()
-                db[name] = {'content' : input.page, 'posted' : posted}
-                web.redirect('/%s' % name)
-        elif input.action == 'Cancel':
-            web.redirect('/%s' % name)
-
-class WikiPage:
-    def GET(self, name):
-        if not name:
-            name = startPage
-        try:
-            doc = db[name]
-            #content = web.utils.safemarkdown(doc['content'])
-            content = markdown(doc['content'])
-            return render.site(name, content)
-        except ResourceNotFound:
-            edit = os.path.join('/edit', name)
-            msg = '''<p>%s does not exist. <a href="%s">Create?</a></p>'''
-            return render.site(name, msg % (name, edit))
-
-if __name__ == "__main__":
-    app.run()
+        return "here we go again! All Hail Britannia!"

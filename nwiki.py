eaf65d921e640f9947b19fa933f194c146e2041d
Nestor G Pestelos Jr <ngpestelos@gmail.com>
2009-03-21 20:03:06 +0800

    new css

--- nwiki.py
+++ nwiki.py
@@ -1,16 +1,35 @@
-import web
+#!/usr/bin/env python
+
+import web, os
 
 urls = (
-  '/w/(.*)', 'StartPage'
+  '/w/(.*)', 'WikiPage',
+  '/w', 'StartPage'
 )
 
 startPage = 'Welcome'
+
+render = web.template.render('static/', base='site')
+
+renderBare = web.template.render('static/')
 
 # don't create docs for these pages
 #watch = ['browse', 'edit', 'about', 'home']
 
 app = web.application(urls, globals())
 
-class StartPage:
+class WikiPage:
     def GET(self, name):
-        return "here we go again! All Hail Britannia!"
+        if not name:
+            raise web.seeother('/w')
+
+        editUrl = os.path.join('/w/edit', name)
+        msg = '<h3 id="notfound">%s does not exist. <a href="%s">Create?</a></h3>'
+        return renderBare.site(name, msg % (name, editUrl))
+
+class StartPage:
+    def GET(self):
+        raise web.seeother('/w/Welcome')
+
+if __name__ == '__main__':
+    app.run()
